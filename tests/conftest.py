26f12b1430384efb7d024e5531c2f7fe6e99fefb
darko <ki14.skakun@gmail.com>
2023-10-15 11:11:22 +0300

    added valid users tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,9 +10,11 @@ from sqlalchemy.orm import sessionmaker
 
 from src.models import Base
 from src.models.user_model import User
+from src.models.token_model import TokenTable
 from src.core.database import get_db
 from src.core.security import hashing_password
 from src.run import app
+from src.services.auth_service import AuthService
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///test.db"
 
@@ -70,3 +72,39 @@ def registered_user(client, user):
     new_user["password"] = user["password"]
 
     return new_user
+
+
+@pytest.fixture
+def list_users(client, user):
+    users = []
+    user["username"] = "registered_1"
+    user["email"] = "register_1@test.com"
+    users.append(user)
+    response = client.post("/users", json=user)
+    assert response.status_code == 201
+    user["username"] = "registered_2"
+    user["email"] = "register_2@test.com"
+    users.append(user)
+    response = client.post("/users", json=user)
+    assert response.status_code == 201
+    return users
+
+
+@pytest.fixture
+def token(registered_user, session):
+    token = AuthService.create_access_token({"id": registered_user["id"]})
+    refresh_token = AuthService.create_refresh_token({"id": registered_user["id"]})
+    token_db = TokenTable(user_id=registered_user["id"], access_token=token, refresh_token=refresh_token, status=True)
+    session.add(token_db)
+    session.commit()
+    session.refresh(token_db)
+    return token
+
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+    return client
--- tests/test_users.py
+++ tests/test_users.py
@@ -2,6 +2,7 @@ from src.schemas import user_schema
 from src.schemas.token_schema import Token
 from jose import jwt
 from src.core.configs import settings
+from typing import List
 
 
 def test_valid_registration(client, user):
@@ -13,10 +14,42 @@ def test_valid_registration(client, user):
 
 def test_valid_login(client, registered_user):
     response = client.post("/users/login", data={"username": registered_user["email"],
-                                                "password": registered_user["password"]})
+                                                 "password": registered_user["password"]})
     login_res = Token(**response.json())
     payload = jwt.decode(login_res.access_token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
     id = payload.get("id")
     assert id == registered_user["id"]
     assert login_res.token_type == "bearer"
     assert response.status_code == 200
+
+
+def test_get_single_user(client, registered_user):
+    response = client.get(f"/users/{1}")
+    user_res = user_schema.UserResp(**response.json())
+    assert response.status_code == 200
+    assert user_res.email == registered_user["email"]
+
+
+def test_get_all_users(client, list_users):
+    response = client.get("/users/list")
+    users = list(map(lambda user: user_schema.UserResp(**user), response.json()))
+    assert response.status_code == 200
+    assert len(users) == len(list_users)
+
+
+def test_update_user(authorized_client, registered_user):
+    registered_user["phone"] = "111-555-7"
+    response = authorized_client.put(f"/users/{registered_user['id']}", json=registered_user)
+    assert response.status_code == 200
+
+
+def test_delete_user(authorized_client, registered_user):
+    response = authorized_client.delete(f"/users/{registered_user['id']}")
+    assert response.status_code == 204
+
+
+def test_logout_user(authorized_client, registered_user):
+    response = authorized_client.get("/users/logout")
+    assert response.status_code == 200
+    response = authorized_client.delete(f"/users/{registered_user['id']}")
+    assert response.status_code == 401
